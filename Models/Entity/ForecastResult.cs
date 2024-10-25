83a2b2da5cab26bd5a37e92dce334ad72b6d0527
Hessam Khajehei <H.khajei@gmail.com>
2024-10-25 11:00:29 +0330

    Initial commit for the new repository

---
+++ Models/Entity/ForecastResult.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace SalesPredictionProject.Models.Entity
+{
+    public class ForecastResult
+    {
+        //[JsonConverter(typeof(CustomDateTimeConverter))]
+        [JsonProperty("ds")] // Map 'ds' in JSON to 'Date' in C#
+
+        public DateTime Date { get; set; }    // Corresponds to 'ds' from the Python API
+        [JsonProperty("yhat")] // Map 'yhat' in JSON to 'PredictedSales' in C#
+
+        public decimal PredictedSales { get; set; }  // Corresponds to 'yhat' (predicted values)
+    }
+
+}
